25b2dfac31e84474db635011493e80f057fbc2d5
Farukh Aleem <farrukhaleem31@gmail.com>
2025-05-18 15:32:46 +0500

    new GTM added

--- app/layout.js
+++ app/layout.js
@@ -25,11 +25,19 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager Head */}
         <Script id="gtm-head" strategy="afterInteractive">
           {`
+
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-TLFSC286');
+
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KF9JD74J');
+
           `}
         </Script>
       </head>
@@ -41,6 +49,14 @@ export default function RootLayout({ children }) {
          <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TLFSC286"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KF9JD74J"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
